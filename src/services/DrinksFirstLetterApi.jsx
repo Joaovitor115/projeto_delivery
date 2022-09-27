8ce7e12fdc61812cef551b992cb270e0899738c7
Yarles de Andrade Espirito Santo <yarles@pop-os.localdomain>
2022-09-27 18:33:42 -0300

    realizado requi 15 grupo-27

--- src/services/DrinksFirstLetterApi.jsx
+++ src/services/DrinksFirstLetterApi.jsx
@@ -2,7 +2,9 @@ const DrinksFirstLetterApi = async (token) => {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${token}`;
   const request = await fetch(url);
   const response = await request.json();
-  return response.drinks;
+  const NUMBER_MAGIC = 11;
+  const currencyDrinks = response.drinks.filter((_d, i) => i <= NUMBER_MAGIC);
+  return currencyDrinks;
 };
 
 export default DrinksFirstLetterApi;
