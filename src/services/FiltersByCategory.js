80afb56aa85bfba02c460657e50c127422715f0b
Reinaldo <reinaldoper83@gmail.com>
2022-09-29 19:44:52 -0300

    ajustando o filterBycategories

--- src/services/FiltersByCategory.js
+++ src/services/FiltersByCategory.js
@@ -2,7 +2,7 @@ export const filterOfMeals = async (category) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
   const data = await response.json();
   const MAGIC_NUMBER = 11;
-  if (data.meals === null) return [];
+  /* if (data.meals === null) return []; */
   return data.meals.filter((_d, i) => i <= MAGIC_NUMBER);
 };
 
@@ -10,7 +10,7 @@ export const filterOfDrinks = async (category) => {
   const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
   const data = await response.json();
   const MAGIC_NUMBER = 11;
-  if (data.drinks === null) return [];
+  /* if (data.drinks === null) return []; */
   const currencyData = data.drinks.filter((_d, i) => i <= MAGIC_NUMBER);
   return currencyData;
 };
